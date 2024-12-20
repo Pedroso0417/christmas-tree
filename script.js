b2323224b121f383d1e233857e7457d3cfb0af09
Pedroso0417/goit-js-hw-8 <136574676+Pedroso0417@users.noreply.github.com>
2024-12-20 16:01:52 +0800

    commit

--- script.js
+++ script.js
@@ -2,29 +2,49 @@ const message = document.getElementById("message");
 const button = document.getElementById("startButton");
 const sparklesContainer = document.querySelector(".sparkles");
 const christmasSong = document.getElementById("christmasSong");
+const modal = document.getElementById("myModal");
+const openModalButton = document.getElementById("openModalButton");
+const closeButton = document.querySelector(".close-button");
 
 button.addEventListener("click", () => {
   message.style.opacity = "1";
-  message.style.transform = "scale(1.2) translateY(0)";
-  message.style.transition = "transform 1s ease, opacity 1s ease";
-
   christmasSong.play();
+
   // Create multiple sparkles for animation
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 100; i++) {
     const sparkle = document.createElement("div");
     sparkle.className = "sparkle";
     sparkle.style.top = `${Math.random() * 100}%`;
     sparkle.style.left = `${Math.random() * 100}%`;
-    sparkle.style.animationDelay = `${Math.random() * 2}s`;
-    sparkles.appendChild(sparkle);
+    sparkle.style.width = "${Math.random() * 5 + 5}px";
+    sparkle.style.height = sparkle.style.width;
+
+    // add random color/
+    const color = ["#FF4500", "#ADFF2F", "#87CEEB", "#FF79B4"];
+
+    sparkle.style.backgroundColor = colors[Math.random() * color.length];
+    sparkle.style.backShadow = "0 0 8px 2px ${sparkle.style.backgroundColor}";
+    // sparkle.style.animationDelay = `${Math.random() * 2}s`;
+    sparklesContainer.appendChild(sparkle);
 
     // Remove sparkles after animation
-    setTimeout(() => sparkle.remove(), 3000);
+    setTimeout(() => sparkle.remove(), 5000);
   }
-  button.disabled = true;
 });
 
-christmasSong.addEventListener("ended", () => {
-  message.style.opacity = "0";
-  button.disabled = false;
+openModalButton.addEventListener("click", () => {
+  modal.style.display = "flex";
 });
+closeButton.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", () => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
+// christmasSong.addEventListener("ended", () => {
+//   message.style.opacity = "0";
+//   button.disabled = false;
+// });
