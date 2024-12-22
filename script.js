2b506c0c2122eaa46b1be380b92717ba5208b2f5
Pedroso0417/goit-js-hw-8 <136574676+Pedroso0417@users.noreply.github.com>
2024-12-22 19:45:28 +0800

    commit

--- script.js
+++ script.js
@@ -66,8 +66,35 @@ function createMeteor() {
     }, 5000);
   }
 }
-
 setInterval(createMeteor, 500);
+function updateCountdown() {
+  const now = new Date();
+  const christmas = new date(now.GetFullYear(), 11, 25);
+  if (now > christmas) {
+    christmas.setFullYear(now.getFullYear() + 1);
+  }
+  const diff = christmas - now;
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor(diff % (1000 * 60)) / 1000;
+
+  document.getElementById("days").textContent = days
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("days").textContent = hours
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("days").textContent = minutes
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("days").textContent = seconds
+    .toString()
+    .padStart(2, "0");
+}
+setInterval(updateCountdown, 1000);
+updateCountdown();
+
 // christmasSong.addEventListener("ended", () => {
 //   message.style.opacity = "0";
 //   button.disabled = false;
